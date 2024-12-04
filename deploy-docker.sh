a4ab968eaf59912133d198eeda43268487057908
coder <usmccoder@gmail.com>
2024-12-04 11:23:09 -0700

    build update

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+#docker buildx create --name multiarch --driver docker-container --use
+#./deploy-docker.sh --no-cache
+
 REGISTRY="sgtcoder"
 PROJECT_NAME="sail"
 PROJECT_TAG="8.3"
 
-#./deploy-docker.sh --no-cache
-
 cd runtimes/$PROJECT_TAG
 
-docker build --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG . || exit 1
+docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG -t $REGISTRY/$PROJECT_NAME:latest . || exit 1
 
-docker push $REGISTRY/$PROJECT_NAME:$PROJECT_TAG
+#docker push --all-tags $REGISTRY/$PROJECT_NAME

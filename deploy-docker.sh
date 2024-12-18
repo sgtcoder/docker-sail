67def6344177b895b8213138ee8ffa4256ec5906
coder <usmccoder@gmail.com>
2024-12-17 21:06:50 -0700

    script update

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -9,6 +9,6 @@ PROJECT_TAG="8.3"
 
 cd runtimes/$PROJECT_TAG
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG -t $REGISTRY/$PROJECT_NAME:latest . || exit 1
+docker buildx build --push --platform linux/amd64,linux/arm64 --build-arg WWWGROUP=1000 $1 -t $REGISTRY/$PROJECT_NAME:$PROJECT_TAG -t $REGISTRY/$PROJECT_NAME:latest ./8.3/Dockerfile || exit 1
 
 #docker push --all-tags $REGISTRY/$PROJECT_NAME
